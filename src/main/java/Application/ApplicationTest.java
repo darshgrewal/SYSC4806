19866ee1777f14d196fc369a121e0bf3257191f2
darshgrewal <grewaldarshpreet@gmail.com>
2019-02-06 11:54:47 -0500

    Added Tests

---
+++ src/main/java/Application/ApplicationTest.java
@@ -0,0 +1,34 @@
+package Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void addressBookEmpty() throws Exception {
+        String url = "/addressbook";
+        this.mockMvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/hal+json;charset=UTF-8"))
+                .andExpect(jsonPath("_embedded.addressBooks", hasSize(0)));
+    }
+
+}
